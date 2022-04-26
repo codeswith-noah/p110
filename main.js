78770d7e6c24c57c64e00e6368e9887c6453ef13
codeswith-noah <85592454+codeswith-noah@users.noreply.github.com>
2022-04-26 16:15:44 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+prediction_1 = "";
+prediction_2 = "";
+
 Webcam.set({
     width:350,
     height:300,
@@ -22,3 +25,47 @@ function modelLoaded(){
     console.log("Model Loaded Successfully...");
 }
 
+function check(){
+    img = document.getElementById('captured_image');
+    classifier.classify(img, gotResult);
+}
+
+function gotResult(error, results){
+    if(error){
+        console.log(error);
+    }
+    else{
+        console.log(results);
+        document.getElementById("result_gesture_name").innerHTML = results[0].label;
+        document.getElementById("result_gesture_name2").innerHTML = results[1].label;
+        prediction_1 = results[0].label;
+        prediction_2 = results[1].label;
+        speak();
+        if(results[0].label == "Thumbs Up") { 
+            document.getElementById("update_gesture").innerHTML = "&#128522;"; 
+        } 
+        if(results[0].label == "Amazing") { 
+            document.getElementById("update_gesture").innerHTML = "&#128532;"; 
+        } 
+        if(results[0].label == "Peace") { 
+            document.getElementById("update_gesture").innerHTML = "&#128548;"; 
+        } 
+        if(results[1].label == "Thumbs Up") { 
+            document.getElementById("update_gesture2").innerHTML = "&#128522;"; 
+        } 
+        if(results[1].label == "Amazing") { 
+            document.getElementById("update_gesture2").innerHTML = "&#128532;"; 
+        } 
+        if(results[1].label == "Peace") { 
+            document.getElementById("update_gesture2").innerHTML = "&#128548;"; 
+        }
+    }
+}
+
+function speak(){
+    var synth = window.speechSynthesis;
+    speak_data_1 = "The first prediction is " + prediction_1;
+    speak_data_2 = "The second prediction is " + prediction_2;
+    var utterThis = new SpeechSynthesisUtterance(speak_data_1 + speak_data_2);
+    synth.speak(utterThis);
+}
